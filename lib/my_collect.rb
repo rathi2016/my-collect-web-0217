5d3ae96464405b7be64d50e89711462f59cdce30
Rathi Nair-Das <rathind14@gmail.com>
2017-01-26 03:16:23 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
-
-
+def my_collect(arr)
+  n = 0
+  arr1 = [ ]
+  while n < arr.length
+    if arr[n].split.length == 2
+      x = arr[n].split.first
+   arr1 << x
+  else
+     x = arr[n].upcase
+    arr1 << x
+  end
+  n+=1
+    end
+    arr1
+end
